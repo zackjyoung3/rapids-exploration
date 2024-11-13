b41c19f887d488ff29a20206c58de5299f2a1fec
zackjyoung3 <zjy@georgetown.edu>
2024-11-13 07:18:04 -0500

    Update PlLfOperation execute_on as we should materialize plan to properly analyze performance

--- rapids_exploration/df_operations/pl_lf_operations/pl_lf_operation.py
+++ rapids_exploration/df_operations/pl_lf_operations/pl_lf_operation.py
@@ -7,5 +7,5 @@ from rapids_exploration.df_operations.df_operation import DFOperation
 
 class PlLfOperation(DFOperation, ABC):
     @abstractmethod
-    def execute_on(self, df: pl.LazyFrame) -> pl.LazyFrame:
+    def execute_on(self, df: pl.LazyFrame) -> pl.DataFrame:
         pass
